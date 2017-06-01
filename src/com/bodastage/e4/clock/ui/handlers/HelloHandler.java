1761dce1114712368ec75573ae12c2736663c5d8
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2017-06-02 00:28:50 +0300

    Creating menus, commands and handlers

---
+++ src/com/bodastage/e4/clock/ui/handlers/HelloHandler.java
@@ -0,0 +1,13 @@
+ 
+package com.bodastage.e4.clock.ui.handlers;
+
+import org.eclipse.e4.core.di.annotations.Execute;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+public class HelloHandler {
+	@Execute
+	public void execute() {
+		MessageDialog.openInformation(null, "Hello", "World");
+	}
+		
+}
