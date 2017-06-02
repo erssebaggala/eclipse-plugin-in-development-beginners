22e12877767bd77bdc11a46856dcbfab8db803bb
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2017-06-03 02:45:37 +0300

    Contributing commands to a popup menu

--- src/com/bodastage/e4/clock/ui/views/TimeZoneTableView.java
+++ src/com/bodastage/e4/clock/ui/views/TimeZoneTableView.java
@@ -11,16 +11,12 @@ import org.eclipse.e4.ui.di.Focus;
 import org.eclipse.e4.ui.services.EMenuService;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
-import org.eclipse.jface.action.Action;
-import org.eclipse.jface.action.MenuManager;
-import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ArrayContentProvider;
 import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Menu;
 import org.eclipse.ui.ISelectionService;
 
 import com.bodastage.e4.clock.ui.internal.TimeZoneDisplayNameColumn;
@@ -30,18 +26,26 @@ import com.bodastage.e4.clock.ui.internal.TimeZoneSummerTimeColumn;
 
 public class TimeZoneTableView {
 	private TableViewer tableViewer;
-
-	@Inject
-	private ISelectionService selectionService;
-
+	
+	@Inject 
+	private ESelectionService selectionService;
+	
 	@PostConstruct
-	public void create(Composite parent) {
-
+	public void create(Composite parent, EMenuService menuService) {
+		
 		tableViewer = new TableViewer(parent, SWT.H_SCROLL | SWT.V_SCROLL);
 		tableViewer.getTable().setHeaderVisible(true);
 		tableViewer.setContentProvider(ArrayContentProvider.getInstance());
 		tableViewer.setInput(ZoneId.getAvailableZoneIds());
-
+		
+		menuService.registerContextMenu(tableViewer.getControl(), "com.bodastage.e4.clock.ui.popupmenu.0");
+		tableViewer.addSelectionChangedListener(event -> {
+			Object selection = ((IStructuredSelection) event.getSelection()).getFirstElement();
+			if(selection != null && selectionService != null ){
+				selectionService.setSelection(selection);
+			}
+		});
+		
 		// Create column before calling setInput()
 		new TimeZoneIDColumn().addColumnTo(tableViewer);
 		new TimeZoneDisplayNameColumn().addColumnTo(tableViewer);
@@ -49,18 +53,6 @@ public class TimeZoneTableView {
 		new TimeZoneSummerTimeColumn().addColumnTo(tableViewer);
 		tableViewer.setInput(ZoneId.getAvailableZoneIds() // get ids
 				.stream().map(ZoneId::of).toArray());
-
-		MenuManager manager = new MenuManager("#PopupMenu");
-		Menu menu = manager.createContextMenu(tableViewer.getControl());
-		tableViewer.getControl().setMenu(menu);
-
-		Action deprecated = new Action() {
-			public void run() {
-				MessageDialog.openInformation(null, "Hello", "World");
-			}
-		};
-		deprecated.setText("Hello");
-		manager.add(deprecated);
 
 	}
 
