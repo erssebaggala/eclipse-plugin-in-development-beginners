a01f22ca780947ce0fd9400e922dc658bf05da18
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2017-06-03 02:59:33 +0300

    UIJob

--- src/com/bodastage/e4/clock/ui/handlers/HelloHandler.java
+++ src/com/bodastage/e4/clock/ui/handlers/HelloHandler.java
@@ -8,12 +8,13 @@ import org.eclipse.core.runtime.jobs.Job;
 import org.eclipse.e4.core.di.annotations.Execute;
 import org.eclipse.e4.ui.di.UISynchronize;
 import org.eclipse.jface.dialogs.MessageDialog;
+import org.eclipse.ui.progress.UIJob;
 
 public class HelloHandler {
 	@Execute
 	public void execute(final UISynchronize display) {
-		Job job = new Job("About to say hello") {
-			protected IStatus run(IProgressMonitor monitor) {
+		UIJob job = new UIJob("About to say hello") {
+			public IStatus runInUIThread(IProgressMonitor monitor) {
 				try {
 					Thread.sleep(5000);
 				} catch (InterruptedException e) {
