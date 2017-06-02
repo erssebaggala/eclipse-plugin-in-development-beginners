a0a7d993e07ab302354858cfca4c4d965935614e
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2017-06-03 02:22:20 +0300

    Add context menu

--- src/com/bodastage/e4/clock/ui/views/TimeZoneTableView.java
+++ src/com/bodastage/e4/clock/ui/views/TimeZoneTableView.java
@@ -8,13 +8,20 @@ import javax.inject.Named;
 
 import org.eclipse.e4.core.di.annotations.Optional;
 import org.eclipse.e4.ui.di.Focus;
+import org.eclipse.e4.ui.services.EMenuService;
 import org.eclipse.e4.ui.services.IServiceConstants;
 import org.eclipse.e4.ui.workbench.modeling.ESelectionService;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.MenuManager;
+import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.ArrayContentProvider;
+import org.eclipse.jface.viewers.IStructuredSelection;
 import org.eclipse.jface.viewers.StructuredSelection;
 import org.eclipse.jface.viewers.TableViewer;
 import org.eclipse.swt.SWT;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Menu;
+import org.eclipse.ui.ISelectionService;
 
 import com.bodastage.e4.clock.ui.internal.TimeZoneDisplayNameColumn;
 import com.bodastage.e4.clock.ui.internal.TimeZoneIDColumn;
@@ -24,8 +31,12 @@ import com.bodastage.e4.clock.ui.internal.TimeZoneSummerTimeColumn;
 public class TimeZoneTableView {
 	private TableViewer tableViewer;
 
+	@Inject
+	private ISelectionService selectionService;
+
 	@PostConstruct
 	public void create(Composite parent) {
+
 		tableViewer = new TableViewer(parent, SWT.H_SCROLL | SWT.V_SCROLL);
 		tableViewer.getTable().setHeaderVisible(true);
 		tableViewer.setContentProvider(ArrayContentProvider.getInstance());
@@ -38,6 +49,18 @@ public class TimeZoneTableView {
 		new TimeZoneSummerTimeColumn().addColumnTo(tableViewer);
 		tableViewer.setInput(ZoneId.getAvailableZoneIds() // get ids
 				.stream().map(ZoneId::of).toArray());
+
+		MenuManager manager = new MenuManager("#PopupMenu");
+		Menu menu = manager.createContextMenu(tableViewer.getControl());
+		tableViewer.getControl().setMenu(menu);
+
+		Action deprecated = new Action() {
+			public void run() {
+				MessageDialog.openInformation(null, "Hello", "World");
+			}
+		};
+		deprecated.setText("Hello");
+		manager.add(deprecated);
 
 	}
 
