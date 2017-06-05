977767ce30f49c35888ed976850ce2118fc15d75
Emmanuel Robert Ssebaggala <emmanuel.ssebaggala@bodastage.com>
2017-06-05 13:17:05 +0300

    Aligning field editors with a grid

--- src/com/bodastage/e4/clock/ui/ClockPreferencePage.java
+++ src/com/bodastage/e4/clock/ui/ClockPreferencePage.java
@@ -1,6 +1,10 @@
 package com.bodastage.e4.clock.ui;
 
+import java.time.ZoneId;
+import java.util.stream.Collectors;
+
 import org.eclipse.core.runtime.preferences.InstanceScope;
+import org.eclipse.jface.preference.ComboFieldEditor;
 import org.eclipse.jface.preference.FieldEditorPreferencePage;
 import org.eclipse.jface.preference.IntegerFieldEditor;
 import org.eclipse.ui.IWorkbench;
@@ -8,6 +12,10 @@ import org.eclipse.ui.IWorkbenchPreferencePage;
 import org.eclipse.ui.preferences.ScopedPreferenceStore;
 
 public class ClockPreferencePage extends FieldEditorPreferencePage implements IWorkbenchPreferencePage {
+
+	public ClockPreferencePage() {
+		super(GRID);
+	}
 
 	@Override
 	public void init(IWorkbench workbench) {
@@ -21,5 +29,8 @@ public class ClockPreferencePage extends FieldEditorPreferencePage implements IW
 		offset.setValidRange(-14, +12);
 		addField(offset);
 
+		String[][] data = ZoneId.getAvailableZoneIds().stream().sorted().map(s -> new String[] { s, s })
+				.collect(Collectors.toList()).toArray(new String[][] {});
+		addField(new ComboFieldEditor("favorite", "Favorite time zone", data, getFieldEditorParent()));
 	}
 }
